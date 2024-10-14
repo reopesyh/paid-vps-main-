8cd97e62f8000c018a30cfb2329cf522724e7c84
reopesyh <hacktest9604@gmail.com>
2024-10-14 20:14:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/VENOM-V2/hr.py",
+    "postStartCommand": "python3 /workspaces/paid-vps-main-/hr.py",
     "customizations": {
         "vscode": {
             "settings": {
